03d4d2ff42e32f21e64efd131387bf425e4938f8
jongmini <jongmin.choi@gmail.com>
2014-03-24 15:40:35 -0700

    pi is runing python script that can make a post to the service layer with soil moisture reading

--- lorax.py
+++ lorax.py
@@ -1,8 +1,12 @@
 # this code reads the soil moisture level and prints the result every 5 seconds
-
+from httplib2 import Http
+from urllib import urlencode
 import spidev
 import time
 import os
+#cpuinfo = open("/proc/cpuinfo","r")
+
+
 
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -16,5 +20,13 @@ def ReadChannel(channel):
 
 while True:
   soil0 = ReadChannel(0)
+  h = Http()		
+  data = dict(reading=soil0,user_id=4,plant_id=3)
+  resp, content = h.request("http://murmuring-crag-3099.herokuapp.com/4/3/"+str(soil0), "POST")
+  resp
+  print resp
+  #print urlencode(data)
+  time.sleep(10)
 
-  time.sleep(5)
+
+
--- post_with_curl.py
+++ post_with_curl.py
@@ -2,10 +2,11 @@ import urllib
 import urllib2
 
 url = "http://murmuring-crag-3099.herokuapp.com/"
-data = { reading: 12345, user_id: 007, plant_id: 4 }
+#data = "{ reading: 12345, user_id: 007, plant_id: 4 }"
 # headers = { "Accept" : "application/json",
 #         "Conthent-Type": "application/json",
 #         "X-Postmark-Server-Token": "abcdef-1234-46cc-b2ab-38e3a208ab2b"}
+data = "reading" : "100"
 req = urllib2.Request(url, data)
 response = urllib2.urlopen(req)
 the_page = response.read()
