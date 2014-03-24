88c68d8e5573464332a405bb769233d75d920010
jongmini <jongmin.choi@gmail.com>
2014-03-24 13:44:51 -0700

    trying to pass the data object again

--- post_with_curl.py
+++ post_with_curl.py
@@ -2,10 +2,10 @@ import urllib
 import urllib2
 
 url = "http://murmuring-crag-3099.herokuapp.com/"
-data = "{reading: 12345, user_id: 007, plant_id: 4 }"
+data = { reading: 12345, user_id: 007, plant_id: 4 }
 # headers = { "Accept" : "application/json",
 #         "Conthent-Type": "application/json",
 #         "X-Postmark-Server-Token": "abcdef-1234-46cc-b2ab-38e3a208ab2b"}
-req = urllib2.Request(url, data, headers)
+req = urllib2.Request(url, data)
 response = urllib2.urlopen(req)
 the_page = response.read()
